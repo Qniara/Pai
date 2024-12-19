6419839f6fdd488defdd8ef6b37f630dddd20e0c
Ziemniak <114557943+Qniara@users.noreply.github.com>
2024-12-19 08:57:39 +0100

    Create spr_19_12_2024.php

---
+++ szkola/Klasa3/sprawdziany/spr_19_12_2024.php
@@ -0,0 +1,28 @@
+<?php
+// $kwota = 123;
+// $nominaly = [1, 2, 5, 10, 20, 50, 100, 200, 500];
+// $length = count($nominaly);
+// $wynik = array_fill(0, $length, 0); 
+// while ($kwota > 0) {
+//     for ($i = $length - 1; $i >= 0; $i--) {
+//         if ($nominaly[$i] <= $kwota) {
+//             $kwota -= $nominaly[$i];
+//             $wynik[$i]++;
+//             break;
+//         }
+//     }
+// }
+// for ($i = 0; $i < count($wynik); $i++) {
+//     if ($wynik[$i] > 0) {
+//         echo $wynik[$i] . " * " . $nominaly[$i] . " zloty<br>";
+//     }
+// }
+
+
+// for($i = 1; $i<=5; $i++){
+//     echo"<center>";
+//     for($j= 1 ; $j<=$i;$j++){
+//         echo"X";
+//     }
+//     echo"</center>";
+// }
