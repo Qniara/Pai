a128af1afc89c936d266badafcdd4f379cecb894
Ziemniak <114557943+Qniara@users.noreply.github.com>
2025-01-02 09:34:03 +0100

    Create cw_02_01_2025.php

---
+++ szkola/Klasa3/cw_02_01_2025.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($servername, $username, $password);
+if (!$conn) {
+    die("Połączenie nieudane: " .mysqli_connect_error());
+}
+$sql = "CREATE DATABASE testDB3";
+if (mysqli_query($conn, $sql)) {
+    echo "Baza danych została pomyślnie utworzona pod nazwą testDB";
+} else {
+    echo "Błąd podczas tworzenia bazy danych: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
+--------------------------------------------------------------------
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($servername, $username, $password);
+if (!$conn) {
+    die("Połączenie nieudane: " .mysqli_connect_error());
+}
+$sql = "CREATE TABLE table(id INT AUTO_INCREMENT PRIMARY KEY, klient_id INT NOT NULL)";
+if (mysqli_query($conn, $sql)) {
+    echo "Baza danych została pomyślnie utworzona pod nazwą testDB";
+} else {
+    echo "Błąd podczas tworzenia bazy danych: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
